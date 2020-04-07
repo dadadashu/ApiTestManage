ebd721001e08073253223c35124a400806a3a290
dadadashu <dadadashu@users.noreply.github.com>
2020-04-07 17:00:40 +0800

    邮件主题改为报告名称

--- app/util/email/SendEmail.py
+++ app/util/email/SendEmail.py
@@ -10,11 +10,12 @@ from .mail_config import EMAIL_PORT, EMAIL_SERVICE
 
 
 class SendEmail(object):
-    def __init__(self, username, password, to_list, file):
+    def __init__(self, username, password, to_list, file, task_name):
         self.Email_service = EMAIL_SERVICE
         self.Email_port = EMAIL_PORT
         self.username = username
         self.password = password
+        self.taskname = task_name
         self.to_list = to_list
         self.file = file
 
@@ -24,14 +25,14 @@ class SendEmail(object):
         message = MIMEMultipart()
         part = MIMEText('Dear all:\n       附件为接口自动化测试报告，此为自动发送邮件，请勿回复，谢谢！', 'plain', 'utf-8')
         message.attach(part)
-        message['From'] = Header("测试组", 'utf-8')
+        message['From'] = Header("接口自动化测试平台", 'utf-8')
         message['To'] = Header(''.join(self.to_list), 'utf-8')
-        subject = '接口测试邮件'
+        subject = self.taskname #'接口测试邮件'
         message['Subject'] = Header(subject, 'utf-8')
 
         # 添加附件
         att1 = MIMEApplication(self.file)
-        att1.add_header('Content-Disposition', 'attachment', filename=('gbk', '', '接口测试报告.html'))
+        att1.add_header('Content-Disposition', 'attachment', filename=('gbk', '', self.taskname+'测试报告.html'))
         message.attach(att1)
 
         try:
